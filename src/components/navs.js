d85f7cfdcc3fe967f26943ba26b9bb90e6fe7aeb
YoussefBayad <jobayad66@gmail.com>
2022-07-17 16:33:07 +0100

    right nav

--- src/components/navs.js
+++ src/components/navs.js
@@ -39,7 +39,7 @@ export default function LeftNav({ ...props }) {
 }
 
 export function RightNav({ ...props }) {
-  const productsItems = [
+  const menuItems = [
     {
       path: "/products",
       label: "PRODUCTS",
@@ -54,19 +54,21 @@ export function RightNav({ ...props }) {
     },
 
     {
-      path: "/products/custom-wigs",
+      path: "/products/custom",
       label: "CUSTOM WIGS",
     },
   ];
   return (
     <Box sx={styles.right} {...props}>
-      <Flex as="ul">
-        {productsItems?.map((item, index) => (
-          <li sx={styles.listItem} key={index}>
-            <Link path={item?.path}>{item?.label}</Link>
-          </li>
-        ))}
-      </Flex>{" "}
+      <Flex as="nav" sx={styles.navbar}>
+        <Box as="ul" sx={styles.navList}>
+          {menuItems?.map((item, index) => (
+            <li sx={styles.listItem} key={index}>
+              <Link path={item?.path}>{item?.label}</Link>
+            </li>
+          ))}
+        </Box>
+      </Flex>
     </Box>
   );
 }
@@ -81,6 +83,43 @@ const styles = {
       p: 0,
     },
   },
+  navbar: {
+    display: "flex",
+    justifySelf: "flex-end",
+    alignItems: "flex-end",
+    height: "75vh",
+    // left: ["5%", "5%", "5%", "5%", "10%", "15%", "15%", "15%"],
+  },
+  navList: {
+    listStyle: "none",
+    marginLeft: "auto",
+    ul: {
+      display: "flex",
+      flexDirection: "column",
+      listStyle: "none",
+      p: 0,
+    },
+    p: 0,
+    ".nav-item": {
+      cursor: "pointer",
+      fontWeight: 400,
+      padding: 0,
+    },
+    ".active": {
+      "&:after": {
+        content: "''",
+        position: "absolute",
+        width: "80px",
+        transform: "scaleX(1)",
+        transformOrigin: "bottom left",
+        height: "2px",
+        bottom: " -5px",
+        left: "0",
+        backgroundColor: "text",
+        transition: "transform 0.25s ease-out",
+      },
+    },
+  },
   listItem: {
     mb: 20,
     cursor: "pointer",
@@ -88,7 +127,7 @@ const styles = {
     "&:after": {
       content: "''",
       position: "absolute",
-      width: "80px",
+      width: "100px",
       transform: " scaleX(0)",
       height: "2px",
       bottom: " -5px",
@@ -103,10 +142,6 @@ const styles = {
     },
   },
   right: {
-    display: "flex",
-    justifySelf: "flex-end",
-    alignItems: "flex-end",
-    height: "75vh",
     ul: {
       display: "flex",
       flexDirection: "column",
