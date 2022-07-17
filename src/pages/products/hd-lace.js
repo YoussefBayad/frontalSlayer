ba93793d37db385157faaf7ab059ae09a6070254
YoussefBayad <jobayad66@gmail.com>
2022-07-17 16:33:57 +0100

    hd lace page dummy data

--- src/pages/products/hd-lace.js
+++ src/pages/products/hd-lace.js
@@ -5,12 +5,109 @@ import SEO from "components/seo";
 import Layout from "components/layout";
 import Products from "sections/products";
 
+import Wig from "assets/images/wig.png";
+import Transparent from "assets/images/transparent.png";
+import Customized from "assets/images/customized.png";
+
+const data = {
+  rowOne: {
+    products: [
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+    ],
+    title: "HD LACE",
+    path: "/products/hd-lace",
+  },
+  rowTwo: {
+    products: [
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+    ],
+    title: "HD LACE",
+    path: "/products/hd-lace",
+  },
+  rowThree: {
+    products: [
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+      {
+        src: Wig,
+        alt: "wig",
+      },
+    ],
+    title: "HD LACE",
+    path: "/products/hd-lace",
+  },
+};
+
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO title="Products" />
-        <Products />
+        <Products data={data} />
       </Layout>
     </ThemeProvider>
   );
