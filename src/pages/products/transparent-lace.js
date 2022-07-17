feaa5a20c8bc211daab9bd4131a63a5c59c57b68
YoussefBayad <jobayad66@gmail.com>
2022-07-17 16:34:08 +0100

    transparent lace page

--- src/pages/products/transparent-lace.js
+++ src/pages/products/transparent-lace.js
@@ -5,12 +5,109 @@ import SEO from "components/seo";
 import Layout from "components/layout";
 import Products from "sections/products";
 
+import Wig from "assets/images/wig.png";
+import Transparent from "assets/images/transparent.png";
+import Customized from "assets/images/customized.png";
+
+const data = {
+  rowOne: {
+    products: [
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+    ],
+    title: "TRANSPARENT LACE",
+    path: "/products/transparent-lace",
+  },
+  rowTwo: {
+    products: [
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+    ],
+    title: "TRANSPARENT LACE",
+    path: "/products/transparent-lace",
+  },
+  rowThree: {
+    products: [
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+      {
+        src: Transparent,
+        alt: "Transparent",
+      },
+    ],
+    title: "TRANSPARENT LACE",
+    path: "/products/transparent-lace",
+  },
+};
+
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO title="Products" />
-        <Products />
+        <Products data={data} />
       </Layout>
     </ThemeProvider>
   );
