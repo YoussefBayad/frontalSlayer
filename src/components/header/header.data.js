019b26e2b2a63154c47efd38dbba9d08fd81cb1e
YoussefBayad <jobayad66@gmail.com>
2022-07-19 18:41:01 +0100

    header nav

--- src/components/header/header.data.js
+++ src/components/header/header.data.js
@@ -6,6 +6,135 @@ export default [
   {
     path: "/products",
     label: "PRODUCTS",
+    isDropdown: true,
+    dropdownItems: [
+      {
+        path: "/products/wigs",
+        label: "WIGS",
+        isDropdown: true,
+        dropdownItems: [
+          {
+            path: "/products/wigs/hd",
+            label: "HD",
+            isDropdown: true,
+            dropdownItems: [
+              {
+                path: "/products/wigs/hd/raw",
+                label: "RAW",
+              },
+              {
+                path: "/products/wigs/hd/virgin",
+                label: "VIRGIN",
+              },
+              {
+                path: "/products/wigs/hd/613",
+                label: "613",
+              },
+            ],
+          },
+          {
+            path: "/products/wigs/transparent",
+            label: "TRANSPARENT",
+            isDropdown: true,
+            dropdownItems: [
+              {
+                path: "/products/wigs/transparent/raw",
+                label: "RAW",
+              },
+              {
+                path: "/products/wigs/transparent/virgin",
+                label: "VIRGIN",
+              },
+              {
+                path: "/products/wigs/transparent/613",
+                label: "613",
+              },
+            ],
+          },
+          {
+            path: "/products/wigs/custom",
+            label: "CUSTOM",
+            isDropdown: true,
+            dropdownItems: [
+              {
+                path: "/products/wigs/custom/hd",
+                label: "HD",
+              },
+              {
+                path: "/products/wigs/custom/transparent",
+                label: "TRANSPARENT",
+              },
+
+              {
+                path: "/products/wigs/custom/waterproof",
+                label: "WATERPROOF",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "/products/bundles",
+        label: "BUNDLES",
+      },
+
+      {
+        path: "/products/closures",
+        label: "CLOSURES",
+        isDropdown: true,
+        dropdownItems: [
+          {
+            path: "/products/closures/hd",
+            label: "HD",
+          },
+          {
+            path: "/products/closures/transparent",
+            label: "TRANSPARENT",
+          },
+        ],
+      },
+      {
+        path: "/products/frontals",
+        label: "FRONTALS",
+        dropdownItems: [
+          {
+            path: "/products/frontals/hd",
+            label: "HD",
+          },
+          {
+            path: "/products/frontals/transparent",
+            label: "TRANSPARENT",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/tools",
+    label: "TOOLS",
+    isDropdown: true,
+    dropdownItems: [
+      {
+        path: "/tools/slay-kit",
+        label: "SLAY KIT",
+      },
+      {
+        path: "/tools/lace-products",
+        label: "LACE PRODUCTS",
+      },
+      {
+        path: "/tools/hair-products",
+        label: "HAIR PRODUCTS",
+      },
+    ],
+  },
+  {
+    path: "/booking",
+    label: "BOOKING",
+  },
+  {
+    path: "/build-a-wig",
+    label: "BUILD A WIG",
   },
   {
     path: "/about",
