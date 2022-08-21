2390ee3b34e33e20481f15fb0e2e87d9b02f7dac
YoussefBayad <jobayad66@gmail.com>
2022-08-21 18:19:47 +0100

    inner height

--- src/sections/products.js
+++ src/sections/products.js
@@ -164,7 +164,7 @@ const styles = {
     position: "absolute",
     transform: "translateX(0%)",
     left: ["calc(0% - 0px)", "calc(0% - 0px)", "calc(0% - 195px)"],
-    bottom: ["18vh"],
+    bottom: ["16vh"],
     zIndex: "3",
   },
   rightFlowers: {
@@ -172,7 +172,7 @@ const styles = {
     position: "absolute",
     transform: "translateX(0%)",
     right: ["calc(0% - 0px)", "calc(0% - 0px)", "calc(0% - 195px)"],
-    bottom: ["18vh"],
+    bottom: ["16vh"],
     zIndex: "3",
   },
 };
