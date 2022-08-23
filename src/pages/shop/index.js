9383b7fe651e9763b36622860f4fb664c3390ade
YoussefBayad <jobayad66@gmail.com>
2022-08-23 02:27:32 +0100

    fix uppercase letter import

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "theme-ui";
 import theme from "theme";
 import SEO from "components/seo";
 import Layout from "components/layout";
-import Shop from "sections/Shop";
+import Shop from "sections/shop";
 
 export default function IndexPage() {
   return (
