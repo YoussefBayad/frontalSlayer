9e23a7ecce369418fb84e8bc2ae8dc6725a9187d
YoussefBayad <jobayad66@gmail.com>
2022-08-29 23:59:51 +0100

    whishlist slic

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,16 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cart/cartSlice';
-import productsReducer from './products/productsSlice';
-import productReducer from './productDetails/productDetailsSlice';
-import authReducer from './auth/authSlice';
-import orderReducer from './order/orderSlice';
-import usersReducer from './users';
-import ordersReducer from './orders';
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cart/cartSlice";
+import wishlistReducer from "./wishlist/wishlistSlice";
+import productsReducer from "./products/productsSlice";
+import productReducer from "./productDetails/productDetailsSlice";
+import authReducer from "./auth/authSlice";
+import orderReducer from "./order/orderSlice";
+import usersReducer from "./users";
+import ordersReducer from "./orders";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
+    wishlist: wishlistReducer,
     products: productsReducer,
     product: productReducer,
     order: orderReducer,
---
+++ src/redux/wishlist/wishlistSlice.js
@@ -0,0 +1,63 @@
+import { createSlice } from "@reduxjs/toolkit";
+import wig from "assets/images/wig.PNG";
+
+const ISSERVER = typeof window === "undefined";
+
+// get data from wishlist
+const data =
+  !ISSERVER && localStorage.getItem("wishlist")
+    ? JSON.parse(localStorage.getItem("wishlist"))
+    : [];
+
+const initialState = {
+  data: [
+    {
+      name: "lace wig",
+      price: 32,
+      _id: "35878",
+      imageUrl: wig,
+    },
+    {
+      name: "lace wig",
+      price: 32,
+      _id: "35878",
+      imageUrl: wig,
+    },
+    {
+      name: "lace wig",
+      price: 32,
+      _id: "35878",
+      imageUrl: wig,
+    },
+  ],
+  loading: false,
+  message: null,
+};
+
+const wishlistSlice = createSlice({
+  name: "wishlist",
+  initialState,
+  reducers: {
+    addToWishlist(state, action) {
+      const product = action.payload;
+      state.data.push(product);
+    },
+    removeFromWishlist(state, action) {
+      state.data = state.data.filter(
+        (product) => product._id !== action.payload
+      );
+    },
+
+    resetWishlist(state, action) {
+      state.data = [];
+    },
+  },
+});
+
+export const {
+  addToWishlist,
+  removeFromWishlist,
+  setLocalStorageItems,
+  resetWishlist,
+} = wishlistSlice.actions;
+export default wishlistSlice.reducer;
