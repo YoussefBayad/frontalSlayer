409c7d6243cc782202821cdbf299e0590fcf0636
YoussefBayad <jobayad66@gmail.com>
2022-09-29 20:37:32 +0100

    next js head tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,6 +30,16 @@ class CustomDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="description of your project" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         </Head>
         <body>
           <Main />
